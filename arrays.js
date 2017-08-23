4b3681c259b6134fb27988f699bab22a65519d2a
Justin Daniel <justinddaniel@gmail.com>
2017-08-23 18:16:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,13 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray() {
+  
+}
+function destructivelyAddElementToBeginningOfArray() {
+  
+}
+function addElementToEndOfArray() {
+  
+}
+function destructivelyAddElementToEndOfArray() {
+  
+}

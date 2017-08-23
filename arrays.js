7d26c9012a2aaa98ea8043ba9d614fe54b9edd08
Justin Daniel <justinddaniel@gmail.com>
2017-08-23 18:52:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   return ["foo", ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift([1], "foo");
+  return array.unshift("foo");
 }
 function addElementToEndOfArray() {
 

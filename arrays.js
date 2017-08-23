e70d18a45911e1de97d78d05719dfc978597843a
Justin Daniel <justinddaniel@gmail.com>
2017-08-23 18:29:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
-return array.unshift("element")
+return ["element", ...array];
 }
 function destructivelyAddElementToBeginningOfArray() {
 

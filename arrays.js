3ed39a7a39f14c2ad3fb5f99bea19e403ccb11d8
Justin Daniel <justinddaniel@gmail.com>
2017-08-23 18:32:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, foo) {
 return ["foo", ...array];
 }
-function destructivelyAddElementToBeginningOfArray() {
-
+function destructivelyAddElementToBeginningOfArray(array, foo) {
+return array.unshift("foo");
 }
 function addElementToEndOfArray() {
 
